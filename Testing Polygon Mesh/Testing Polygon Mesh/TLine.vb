3cdf402277f48274e92af5dd8db5f5bea925513f
Kevin Jordan <kevincentdeco@gmail.com>
2018-03-26 10:16:36 +0700

    test

--- Testing Polygon Mesh/Testing Polygon Mesh/TLine.vb
+++ Testing Polygon Mesh/Testing Polygon Mesh/TLine.vb
@@ -1,14 +1,20 @@
 ﻿Public Class TLine
     Public Point1 As TPoint
     Public Point2 As TPoint
+    Public Poly1 As Integer
+    Public Poly2 As Integer
 
     Public Sub New()
         Point1 = New TPoint()
         Point2 = New TPoint()
+        Poly1 = Nothing
+        Poly2 = Nothing
     End Sub
 
-    Public Sub New(ByRef Point1 As TPoint, ByRef Point2 As TPoint)
+    Public Sub New(ByRef Point1 As TPoint, ByRef Point2 As TPoint, p1 As Integer, p2 As Integer)
         Me.Point1 = Point1
         Me.Point2 = Point2
+        Poly1 = p1
+        Poly2 = p2
     End Sub
 End Class
---
+++ Testing Polygon Mesh/Testing Polygon Mesh/TObject.vb
@@ -0,0 +1,3 @@
+﻿Public Class TObject
+
+End Class
