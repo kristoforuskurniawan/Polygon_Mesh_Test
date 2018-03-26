177c755f08bd98d1fe9960f9269b53b945962cd1
kristoforuskurniawan <kristoforus10@yahoo.com>
2018-03-26 10:23:08 +0700

    Creating Data Structure.

--- Testing Polygon Mesh/Testing Polygon Mesh/Form1.vb
+++ Testing Polygon Mesh/Testing Polygon Mesh/Form1.vb
@@ -1,11 +1,17 @@
 ﻿Public Class MainForm
 
     Private bitmapCanvas As Bitmap
-    Private PointsList As List(Of TPoint)
+    Private PointsArr As List(Of TPoint)
+    Private EdgesArr As List(Of TLine)
+    Private MeshesArr As List(Of TMesh)
+    Private MeshList As TMeshList
 
     Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         bitmapCanvas = New Bitmap(MainCanvas.Width, MainCanvas.Height)
-        PointsList = New List(Of TPoint)
+        PointsArr = New List(Of TPoint)
+        EdgesArr = New List(Of TLine)
+        MeshesArr = New List(Of TMesh)
+        MeshList = New TMeshList()
     End Sub
 
     Private Sub DrawMeshButton_Click(sender As Object, e As EventArgs) Handles DrawMeshButton.Click
--- Testing Polygon Mesh/Testing Polygon Mesh/TMeshList.vb
+++ Testing Polygon Mesh/Testing Polygon Mesh/TMeshList.vb
@@ -1,3 +1,7 @@
 ﻿Public Class TMeshList
     Public MeshList As List(Of TMesh)
+
+    Public Sub New()
+        MeshList = New List(Of TMesh)
+    End Sub
 End Class
