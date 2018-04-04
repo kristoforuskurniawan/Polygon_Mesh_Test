9c7a04db52982e13f6898c0f2bfbdd964ed123a6
Kevin Jordan <kevincentdeco@gmail.com>
2018-04-04 21:16:11 +0700

    justasphere

--- Testing Polygon Mesh/Testing Polygon Mesh/Form1.vb
+++ Testing Polygon Mesh/Testing Polygon Mesh/Form1.vb
@@ -1,6 +1,8 @@
 ﻿Public Class MainForm
 
     Private bitmapCanvas As Bitmap
+    Private g As Graphics
+    Private blackpen As Pen
     Private PointsArr As List(Of TPoint)
     Private EdgesArr As List(Of TLine)
     Private MeshesArr As List(Of TMesh)
@@ -9,6 +11,8 @@
 
     Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         bitmapCanvas = New Bitmap(MainCanvas.Width, MainCanvas.Height)
+        g = Graphics.FromImage(bitmapCanvas)
+        blackpen = New Pen(Color.Black, 0.5)
         MainCanvas.Image = bitmapCanvas
         PointsArr = New List(Of TPoint)
         EdgesArr = New List(Of TLine)
@@ -17,8 +21,68 @@
         sphereRadius = 0
     End Sub
 
-    Private Sub DrawMeshButton_Click(sender As Object, e As EventArgs) Handles DrawMeshButton.Click
 
+    Private Function dotproduct(x As Double(), y As Double()) As Double
+        Dim d As Double = x(0) * y(0) + x(1) * y(1) + x(2) * y(2)
+        Return If(d < 0, -d, 0)
+    End Function
+
+    Dim point1, point2, point3, point4, point5 As System.Drawing.Point
+
+    Private Sub DrawMeshButton_Click(sender As Object, e As EventArgs) Handles DrawMeshButton.Click
+        MainCanvas.Refresh()
+        g = MainCanvas.CreateGraphics
+        Dim i, r As Integer
+        Dim x, y, z, s, q, p, px, py As Single
+        Dim mx(600), my(600) As Integer
+        s = PI / 4
+        r = 100
+        i = 1
+        q = -PI / 2 + 0.1
+        For p = 0 To 2 * PI Step 0.2
+            x = r * Math.Cos(q) * Math.Sin(p)
+            y = r * Math.Sin(q)
+            z = r * Math.Cos(q) * Math.Cos(p)
+            px = x * 1.2
+            py = y - z * Math.Sin(s)
+            mx(i) = px + 200
+            my(i) = 150 - py
+            i = i + 1
+        Next p
+        point1.X = mx(1) : point1.Y = my(1)
+        For q = -PI / 2 + 0.2 To PI / 2 Step 0.2
+            i = 1
+            For p = 0 To 2 * PI Step 0.2
+                x = r * Math.Cos(q) * Math.Sin(p)
+                y = r * Math.Sin(q)
+                z = r * Math.Cos(q) * Math.Cos(p)
+                px = x * 1.2
+                py = y - z * Math.Sin(s)
+
+                point2.X = px + 200 : point2.Y = 150 - py
+                g.DrawLine(blackpen, point1, point2)
+
+                point1 = point2
+
+                point3.X = mx(i) : point3.Y = my(i)
+                point4.X = px + 200 : point4.Y = 150 - py
+                g.DrawLine(blackpen, point3, point4)
+                point3 = point4
+                mx(i) = px + 200
+                my(i) = 150 - py
+                i = i + 1
+            Next p
+            p = 0
+            x = r * Math.Cos(q) * Math.Sin(p)
+            y = r * Math.Sin(q)
+            z = r * Math.Cos(q) * Math.Cos(p)
+            px = x * 1.2
+            py = y - z * Math.Sin(s)
+
+            point5.X = px + 200 : point5.Y = 150 - py
+            g.DrawLine(blackpen, point4, point5)
+            point4 = point5
+        Next q
     End Sub
 
     Private Sub MainCanvas_Click(sender As Object, e As EventArgs) Handles MainCanvas.Click
