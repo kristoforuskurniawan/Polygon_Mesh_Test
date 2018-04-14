ce9e5b758a97fd830a86abe58e7f6afdad8636d6
kristoforuskurniawan <kristoforus10@yahoo.com>
2018-04-14 13:56:26 +0700

    Akhirnya bola bisa

--- Testing Polygon Mesh/Testing Polygon Mesh/TArrPoint.vb
+++ Testing Polygon Mesh/Testing Polygon Mesh/TArrPoint.vb
@@ -1,14 +1,11 @@
 ﻿Public Class TArrPoint
-    Public x, y, z, w As Double
-End Class
-Public Class ListPoints
     Public N As Integer
     Public Elmt() As TPoint
     Public Sub Init()
         N = 0
         ReDim Elmt(-1)
     End Sub
-    Public Overloads Sub InsertPoint(x As Double, y As Double, z As Double)
+    Public Overloads Sub InsertLast(x As Double, y As Double, z As Double)
         Dim P As TPoint
         P = New TPoint
         P.x = x
@@ -18,4 +15,8 @@ Public Class ListPoints
         Elmt(N) = P
         N = N + 1
     End Sub
+
+    Public Function Count()
+        Return N
+    End Function
 End Class
