36419c49e9247f459207c43edb1b9f169ac34eca
Ardy Wijaya <ardywijaya1997@gmail.com>
2018-04-14 15:11:03 +0700

    test

--- Testing Polygon Mesh/Testing Polygon Mesh/TPoint.vb
+++ Testing Polygon Mesh/Testing Polygon Mesh/TPoint.vb
@@ -1,17 +1,3 @@
 ﻿Public Class TPoint
     Public x, y, z, w As Double
-
-    Public Sub New()
-        x = 0
-        y = 0
-        z = 0
-        w = 1
-    End Sub
-
-    Public Sub New(ByVal x As Double, ByVal y As Double, ByVal z As Double)
-        Me.x = x
-        Me.y = y
-        Me.z = z
-        w = 1
-    End Sub
 End Class
